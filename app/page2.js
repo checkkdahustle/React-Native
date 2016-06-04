0372d63cae7ba127ef64e1b5c69b023c05fe9454
checkkdahustle <checkkdahustle@fullsail.edu>
2016-06-03 21:24:19 -0400

    added touchableOpacity component

--- app/page2.js
+++ app/page2.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 
 import {
+
   Text,
   View,
   StyleSheet,
-  TouchableHighlight
+  // TouchableHighlight,
+  TouchableOpacity
 } from 'react-native';
 
 import Header from './components/Header';
+// import ImageEx from './components/image';
 
 class Second extends Component {
 
   buttonBack(){
     this.props.navigator.pop();
-  }
+  }//close buttonBack
 
   render(){
 
@@ -22,20 +25,28 @@ class Second extends Component {
     return (
       <View style={styles.container}>
         <Header
-          title={this.props.title}
-          />
-          <TouchableHighlight
-            style={styles.button}
-            onPress={this.buttonBack.bind(this)}
-          >
-            <Text style={styles.buttonText}>Back to Main</Text>
-          </TouchableHighlight>
-        <Text style={styles.text}>This is the second components</Text>
+          title={this.props.title} />
 
-      </View>
-    )
-  }
-}
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.buttonBack.bind(this)} >
+
+            <Text style={styles.buttonText}>Back to Main</Text>
+          </TouchableOpacity>
+        <Text style={styles.text}>This second components has TouchableOpacity </Text>
+        </View>
+  // <TouchableHighlight
+  //         onPress={this._onPressButton}>
+  //
+  //             <Text>Something</Text>
+  //         </TouchableHighlight>
+
+
+// Trying to add pic icon
+
+    )//return
+  }//close render
+}//close component class 'Second'
 
 const styles = StyleSheet.create({
   container: {
