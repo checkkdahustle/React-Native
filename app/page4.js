9caa228b1fcf57dc3110f3d8946f24072c04924f
checkkdahustle <checkkdahustle@fullsail.edu>
2016-06-05 23:00:26 -0400

    added more text inputs and styles

--- app/page4.js
+++ app/page4.js
@@ -12,6 +12,13 @@ import Header from './components/Header';
 
 class Fourth extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }//close constructor
+
   buttonBack(){
     this.props.navigator.pop();
   }
@@ -24,50 +31,79 @@ class Fourth extends Component {
       <View style={styles.container}>
         <Header
           title={this.props.title}
-          />
-          <TextInput
-            autoCapitalize="none"
-            placeholder="Enter text in this TextInput"
-            style={styles.InputText}
-            onPress={this.buttonBack.bind(this)}
-          >
-          </TextInput>
+        />
+          <View>
+            <TextInput
+              style={styles.InputText}
+              onChangeText={(text) => this.setState({text})}
+              // value={this.state.text}
+              autoCapitalize="none"
+              placeholder="Enter text in this Input"
+            />
+            <TextInput
+              style={styles.InputText}
+              onChangeText={(text) => this.setState({text})}
+              // value={this.state.text}
+              autoCapitalize="none"
+              placeholder="Enter some more text in this Input"
+            />
+            <TextInput
+              style={styles.InputText}
+              onChangeText={(text) => this.setState({text})}
+              // value={this.state.text}
+              autoCapitalize="none"
+              placeholder="Enter ... you get the point"
+            />
+          </View>
+
           <TouchableHighlight
-            style={styles.button}
-            onPress={this.buttonBack.bind(this)}
-          >
-            <Text style={styles.buttonText}>Back to Main</Text>
+            onPress={this.buttonBack.bind(this)} >
+
+            <View style= {styles.button} >
+              <Text style={styles.buttonText}>
+                Back to Main
+              </Text>
+            </View>
           </TouchableHighlight>
 
-        <Text style={styles.text}>This fourth component is a TextInput</Text>
 
+        <Text style={styles.text}>
+          This fourth component is a TextInput
+        </Text>
       </View>
-    )
-  }
-}
+    )//return
+  }//close render
+}//close component class 'Fourth'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   text: {
     color: 'red',
   },
   button: {
-    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#050',
+    backgroundColor: '#07d',
+    width: 340,
+		height: 100,
+    borderRadius: 10,
   },
   buttonText: {
-    color: '#fff'
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 35,
   },
   InputText: {
+    alignItems: 'center',
+    width: 350,
     height: 40,
     borderColor: 'gray',
-    borderWidth: 1
+    borderWidth: .5,
+    marginBottom: 25,
+
   }
 });
 
